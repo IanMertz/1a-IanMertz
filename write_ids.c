15a104d47936c71b90df4b9b6736970c10c70171
Ian Mertz <ianmmertz@gmail.com>
2024-09-13 16:42:22 -0400

    ChangedTheId

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "IanMertz";
+    char STUDENT_ID[] = "997952849";
 
     return 0 ;
 }
